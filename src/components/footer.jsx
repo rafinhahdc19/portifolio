bdf56fd4a58364c8d243aedaaed1c8eb13a00510
rafinhahdc19 <rafinhacarvalhorlc@gmail.com>
2024-03-05 20:24:34 -0300

    First commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
     const anoAtual = new Date().getFullYear();
     return (
         <>
-            <div className=" p-6 pb-[4rem] ">
-                <div className="lg:max-w-7xl  mr-auto ml-auto">
+            <div className=" md:p-6 md:pb-[4rem] ">
+                <div className="lg:max-w-7xl  md:mr-auto ml-auto">
                         
                 </div>
             </div>
